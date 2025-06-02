ad1ea16d17f7296147c50f92945b3ab52b833906
dowsky <vasile.bezer.1@gmail.com>
2025-06-02 17:34:06 +0300

    renamed dir

--- part1/intro-to-react/src/App.jsx
+++ part1/intro-to-react/src/App.jsx
@@ -1,24 +1,25 @@
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
-  const content = [ part1, part2, part3 ]
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   return (
     <div>
       <Header course = {course} />
-      <Content content = {content} />
-      <Total total = {content.reduce((sum, item) => sum + item.exercises, 0)} />
+      <Content content = {parts} />
+      <Total total = {parts.reduce((sum, item) => sum + item.exercises, 0)} />
     </div>
   )
 }
