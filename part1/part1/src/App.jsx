f84e6ff7890f2b977bd02ee30b155443a0b718de
dowsky <vasile.bezer.1@gmail.com>
2025-06-02 14:33:07 +0300

    ex 1.3

--- part1/part1/src/App.jsx
+++ part1/part1/src/App.jsx
@@ -1,22 +1,24 @@
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
-  const content = [
-    {name: part1, exercises: exercises1}
-    ,{name: part2, exercises: exercises2}
-    ,{name: part3, exercises: exercises3}
-  ]
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
+  const content = [ part1, part2, part3 ]
 
   return (
     <div>
       <Header course = {course} />
       <Content content = {content} />
-      <Total total = {exercises1 + exercises2 + exercises3} />
+      <Total total = {content.reduce((sum, item) => sum + item.exercises, 0)} />
     </div>
   )
 }
