db8faf78f00639f5b0ade4f4d92a9af7874b05c2
dowsky <vasile.bezer.1@gmail.com>
2025-06-02 18:36:36 +0300

    ex 1.8

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -5,6 +5,7 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  
 
   return (
     <div>
@@ -22,17 +23,23 @@ const App = () => {
           bad
         </button>
       </div>
-      <div>
-        <h1>
-          statistics
-        </h1>
-        <p>good {good}</p>
-        <p>neutral {neutral}</p>
-        <p>bad {bad}</p>
-        <p>total {good + neutral + bad}</p>
-        <p>avg. {(good - bad) / (good + neutral + bad)}</p>
-        <p>positive % {(good) * 100 / (good + neutral + bad)}</p>
-      </div>
+      <Statistics good = {good} bad = {bad} neutral ={neutral} />
+    </div>
+  )
+}
+
+const Statistics = ({ good, bad, neutral }) => {
+  return (
+    <div>
+      <h1>
+        statistics
+      </h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>total {good + neutral + bad}</p>
+      <p>avg. {(good - bad) / (good + neutral + bad)}</p>
+      <p>positive % {(good) * 100 / (good + neutral + bad)}</p>
     </div>
   )
 }
