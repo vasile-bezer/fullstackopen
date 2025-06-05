987545d5eb4d3bf927eba7a1ceb8785e36e2953a
dowsky <vasile.bezer.1@gmail.com>
2025-06-05 10:14:05 +0300

    ex 2.10

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -13,7 +13,7 @@ const App = () => {
 
 	const submitHandler = (event) => {
 		event.preventDefault();
-		
+
 		const newPerson = { name: newName, number: newNumber }
 		if ( persons.some((person => JSON.stringify(person) === JSON.stringify(newPerson))) ) { return alert(`${newName} is already in the phonebook`); }
 		if ( !!newName ){ setPersons(persons.concat(newPerson)); }
@@ -24,23 +24,56 @@ const App = () => {
 	return (
 		<div>
 			<h2>Phonebook</h2>
-				<div>
-					filter: <input onChange={(event) => setFilter(event.target.value)} value={filter}/>
-				</div>
+			<Filter filter={filter} setFilter={setFilter}/>
 			<h2>add a new</h2>
-				<form onSubmit={submitHandler}>
-					<div>
-						name: <input onChange={(event) => setNewName(event.target.value)} value={newName}/>
-						number: <input onChange={(event) => setNewNumber(event.target.value)} value={newNumber}/>
-					</div>
-					<div>
-					<button type="submit">add</button>
-					</div>
-				</form>
+			<PersonForm 
+				newName={newName}
+				setNewName={setNewName}
+				newNumber={newNumber}
+				setNewNumber={setNewNumber}
+				submitHandler={submitHandler}
+			/>
 			<h2>Numbers</h2>
-			{ filteredPersons.map( person => (<p key={person.name}>{person.name} {person.number}</p>) ) }
+			<Persons filteredPersons={filteredPersons}/>
 		</div>
 	)
 }
+
+const Filter = (props) => {
+	const {filter, setFilter} = props;
+
+	return (
+		<div>
+			filter: <input onChange={(event) => setFilter(event.target.value)} value={filter}/>
+		</div>
+	)
+}
+
+const PersonForm = (props) => {
+	const {newName, setNewName} = props;
+	const {newNumber, setNewNumber} = props;
+	const {submitHandler} = props;
+
+	return (
+		<form onSubmit={submitHandler}>
+			<div>
+				name: <input onChange={(event) => setNewName(event.target.value)} value={newName}/>
+				number: <input onChange={(event) => setNewNumber(event.target.value)} value={newNumber}/>
+			</div>
+			<div>
+			<button type="submit">add</button>
+			</div>
+		</form>
+	)
+}
+
+const Persons = (props) => {
+	const {filteredPersons} = props;
+
+	return (
+		<div>{ filteredPersons.map( person => (<p key={person.name}>{person.name} {person.number}</p>) ) }</div>
+	)
+}
+
 
 export default App
