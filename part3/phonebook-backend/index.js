01b22b34a029e2b37a62b586ba2d334b5ef2a155
dowsky <vasile.bezer.1@gmail.com>
2025-07-14 00:43:08 +0300

    ex 3.8

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -3,7 +3,14 @@ const morgan = require('morgan')
 
 const app = express()
 app.use(express.json())
-app.use(morgan('tiny'))
+morgan.token('post-data', (req) => {
+  if (req.method === 'POST') {
+    return JSON.stringify(req.body)
+  }
+  return ''
+})
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :post-data'))
+
 
 let persons = [
 	{ 
