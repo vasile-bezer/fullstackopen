b2d3058693e50b2e39fdd27806075b904f598443
dowsky <vasile.bezer.1@gmail.com>
2025-07-14 00:18:17 +0300

    ex 3.5

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,28 +1,31 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let persons = [
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
+	{ 
+		"id": "1",
+		"name": "Arto Hellas", 
+		"number": "040-123456"
+	},
+	{ 
+		"id": "2",
+		"name": "Ada Lovelace", 
+		"number": "39-44-5323523"
+	},
+	{ 
+		"id": "3",
+		"name": "Dan Abramov", 
+		"number": "12-43-234345"
+	},
+	{ 
+		"id": "4",
+		"name": "Mary Poppendieck", 
+		"number": "39-23-6423122"
+	}
 ]
+
 
 app.get('/api/persons', (request, response) => {
 	response.json(persons)
@@ -39,11 +42,11 @@ app.get('/api/persons/:id', (request, response) => {
 })
 
 app.get('/info', (request, response) => {
-    const count = persons.length
-    const time = new Date()
-    response.send(
-        `<p>Phonebook has info for ${count} people</p><p>${time}</p>`
-    )
+	const count = persons.length
+	const time = new Date()
+	response.send(
+		`<p>Phonebook has info for ${count} people</p><p>${time}</p>`
+	)
 })
 
 app.delete('/api/persons/:id', (request, response) => {
@@ -51,6 +54,26 @@ app.delete('/api/persons/:id', (request, response) => {
 	persons = persons.filter(person => person.id !== id)
 
 	response.status(204).end()
+})
+
+app.post('/api/persons', (request, response) => {
+	const body = request.body
+
+	if (!body.name || !body.number) {
+		return response.status(400).json({ 
+			error: 'content missing' 
+		})
+	}
+
+	const person = {
+		name: body.name,
+		number: body.number,
+		id: Math.floor(Math.random() * 1000000),
+	}
+
+	persons = persons.concat(person)
+
+	response.json(person)
 })
 
 const PORT = 3001
