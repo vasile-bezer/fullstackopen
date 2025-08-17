866c00bd558249671b182e8f391d9b14edb82f20
dowsky <vasile.bezer.1@gmail.com>
2025-08-17 12:56:35 +0300

    ex 3.15 - ex 3.18

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -21,39 +21,52 @@ morgan.token('post-data', (req) => {
 })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :post-data'))
 
-
-
 app.get('/api/persons', (request, response) => {
 	Person.find({}).then(persons => {
 		response.json(persons)
 	})
 })
 
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
 	Person.findById(request.params.id).then(person => {
 		if (person) {
 			response.json(person)
 		} else {
 			response.status(404).end()  
 		}
-	})
+	}).catch(error => next(error))
 })
 
 app.get('/info', (request, response) => {
-	Person.countDocuments({})
-		.then(count => {
-			const time = new Date()
-			response.send(
-				`<p>Phonebook has info for ${count} people</p><p>${time}</p>`
-			)
-		})
+	Person.countDocuments({}).then(count => {
+		const time = new Date()
+		response.send(
+			`<p>Phonebook has info for ${count} people</p><p>${time}</p>`
+		)
+	})
 })
 
+app.put('/api/persons/:id', (request, response, next) => {
+	const { name, number } = request.body
 
-app.delete('/api/persons/:id', (request, response) => {
-	Person.findByIdAndDelete(request.params.id).then(() => {
-			response.status(204).end()
+	Person.findById(request.params.id).then(person => {
+		if (!person) {
+			return response.status(404).end()
+		}
+
+		person.name = name
+		person.number = number
+
+		return person.save().then((updatedPerson) => {
+			response.json(updatedPerson)
 		})
+	}).catch(error => next(error))
+})
+
+app.delete('/api/persons/:id', (request, response, next) => {
+	Person.findByIdAndDelete(request.params.id).then(() => {
+		response.status(204).end()
+	}).catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response) => {
@@ -72,6 +85,18 @@ app.post('/api/persons', (request, response) => {
 		response.json(savedPerson)
 	})
 })
+
+const errorHandler = (error, request, response, next) => {
+	console.error(error.message)
+
+	if (error.name === 'CastError') {
+		return response.status(400).send({ error: 'malformatted id' })
+	} 
+
+	next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
--- part3/phonebook-backend/mongo.js
+++ part3/phonebook-backend/mongo.js
@@ -1,24 +1,17 @@
+require('dotenv').config()
+
 const mongoose = require('mongoose')
 
-if (process.argv.length < 3) {
-  console.log('give password as argument')
-  process.exit(1)
-}
-
-const password = process.argv[2]
-
-const url = `mongodb+srv://admin:${password}@cluster0.bwqaa3r.mongodb.net/phonebook?retryWrites=true&w=majority&appName=Cluster0`
-
 mongoose.set('strictQuery',false)
+mongoose.connect(process.env.MONGODB_URI)
 
-mongoose.connect(url)
+const Person = mongoose.model(
+  'Person', new mongoose.Schema({
+    name: String,
+    number: String,
+  })
+)
 
-const personSchema = new mongoose.Schema({
-  name: String,
-  number: String,
-})
-
-const Person = mongoose.model('Person', personSchema)
 switch (process.argv.length) {
   case 3:
     Person
